0ecd66e02540af562dd1e534d639ff475176a4af
Manuel Marin <manuelmarin1994@gmail.com>
2022-01-13 20:28:24 -0400

    Setting and connect app with mongodb

--- config/db.js
+++ config/db.js
@@ -4,7 +4,6 @@ const { dbUsername, dbPassword, dbHost } = require('./');
 const uri = `mongodb+srv://${dbUsername}:${dbPassword}@${dbHost}/SysInB`;
 
 const connection = async ()=>{
-  console.log(uri);
   const conn = await mongoose.connect(uri);
   console.log("Mongo DB Connected",conn.connection.host);
 };
