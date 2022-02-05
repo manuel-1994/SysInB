9ac0311d48d9bed740a09f94e15b0137b5f8c431
Manuel Marin <manuelmarin1994@gmail.com>
2022-02-05 09:52:04 -0400

    middleware restructuring

--- src/middleware/validateEmail.js
+++ src/middleware/validateEmail.js
@@ -2,11 +2,15 @@ const Clients = require("../services/clients.service");
 const Providers = require("../services/providers.service");
 const Users = require("../services/users.service");
 
-const validateEmail = async ({user,client,provider},req,res,next) =>{
-  const emailExist = user? await new Users().getUser({email:req.body.email}):
-                   client? await new Clients().getClient({email:req.body.email}):
-                           await new Providers().getProvider({email:req.body.email});  
-                                           
+const emailVerify = {
+  user: async (email) => await new Users().getUser({email}),
+  client:  async (email) => await new Clients().getClient({email}),
+  provider: async (email) => await new Providers().getProvider({email})
+}
+
+const validateEmail = async (param,req,res,next) =>{
+    const emailExist = await emailVerify[param](req.body.email);
+
     if(emailExist){
     return res.status(400).json({
     data:emailExist, 
@@ -17,15 +21,15 @@ const validateEmail = async ({user,client,provider},req,res,next) =>{
 }
 
 const user = (req,res,next)=>{
-  validateEmail({user:true}, req,res,next)
+  validateEmail("user", req,res,next)
 }
 
 const client = (req,res,next)=>{
-  validateEmail({client:true}, req,res,next)
+  validateEmail("client", req,res,next)
 }
 
 const provider = (req,res,next)=>{
-  validateEmail({provider:true}, req,res,next)
+  validateEmail("provider", req,res,next)
 }
 
 module.exports = {
